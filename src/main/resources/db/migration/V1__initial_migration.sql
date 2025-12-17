1ced7c743f61afc71de5ad0019fbc23499fc3f08
Fenera Taye <fenerataye@gmail.com>
2025-12-17 17:12:54 -0600

    added new tables to db

--- src/main/resources/db/migration/V1__initial_migration.sql
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -1,7 +1,6 @@
 create table users
 (
-    id       bigint auto_increment
-        primary key,
+    id       bigint auto_increment      primary key,
     name     varchar(255) not null,
     email    varchar(255) not null,
     password varchar(255) not null
---
+++ src/main/resources/db/migration/V5__add_tags_add_user_tags.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tags
+(
+    id      INT     PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_tags
+(
+    user_id     BIGINT     NOT NULL,
+    tag_id  BIGINT  NOT NULL,
+    PRIMARY KEY (user_id, tag_id),
+    # if user row deleted in users table, deleted in user_tags also
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    # same for tags & user_tags
+    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
